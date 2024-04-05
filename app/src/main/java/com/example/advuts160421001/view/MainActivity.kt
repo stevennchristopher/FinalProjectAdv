d6e0197b38fdc5d59425735a7314bd4c387eeafd
stevennchristopher <stchrist03@gmail.com>
2024-04-05 23:55:29 +0700

    add popup nav

--- app/src/main/java/com/example/advuts160421001/view/MainActivity.kt
+++ app/src/main/java/com/example/advuts160421001/view/MainActivity.kt
@@ -3,15 +3,26 @@ package com.example.advuts160421001.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.advuts160421001.R
 import com.example.advuts160421001.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        navController = (supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment).navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
+
 }
