4f8267c707e7edb796f7b703cfe940ccd5dd5149
cenz19 <vincenthadinata19@gmail.com>
2024-06-29 14:56:16 +0700

    Added Interface file, update previous file too

---
+++ app/src/main/java/com/example/advuts160421001/view/Interfaces.kt
@@ -0,0 +1,10 @@
+package com.example.advuts160421001.view
+
+import android.view.View
+import android.widget.CompoundButton
+import com.example.advuts160421001.model.Berita
+
+interface ReadButtonClick {
+    fun onReadButtonClick(v: View)
+}
+
